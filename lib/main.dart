3553f3a5d4171028534b4e101c7193245b5ccfbe
Richard Gusmao <richardgusmaoalberto@gmail.com>
2023-12-18 11:55:40 +0100

    Fixing bug on Login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:inventory/screens/home_screen.dart';
+import 'package:inventory/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: "Inventory App",
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: LoginScreen(),
     );
   }
 }
